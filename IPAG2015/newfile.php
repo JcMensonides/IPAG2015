4fcd70058f46e8ee0bc6bc94b2c3efb12a60a663
arnaudJacquez <jacquez.arnaud@gmail.com>
2015-04-23 15:18:06 +0200

    premier test

--- IPAG2015/newfile.php
+++ IPAG2015/newfile.php
@@ -1,15 +1,15 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
 <head>
-<title>Notre première instruction : echo</title>
+<title>Notre premiï¿½re instruction : echo</title>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
 </head>
 <body>
-	<h2>Affichage de texte avec PHP</h2>
+	<h2>test php 1ere branche</h2>
 
 	<p>
-		Cette ligne a été écrite entièrement en HTML.<br />
-            <?php echo "Celle-ci a été écrite entièrement en PHP."; ?>
+		Cette ligne a ï¿½tï¿½ ï¿½crite entiï¿½rement en HTML.<br />
+            <?php echo "Celle-ci a ï¿½tï¿½ ï¿½crite entiï¿½rement en PHP."; ?>
         </p>
 </body>
 </html>
