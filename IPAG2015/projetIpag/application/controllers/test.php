37f8faf24ff768bef6621c8114afc41cb76ad089
arnaudJacquez <jacquez.arnaud@gmail.com>
2015-04-27 11:16:15 +0200

    Mise en place CodeIgniter

---
+++ IPAG2015/projetIpag/application/controllers/test.php
@@ -0,0 +1,12 @@
+<?php
+
+class Test extends CI_Controller
+{
+    public function index()
+    {
+        $this->output->enable_profiler(true);
+    }
+}
+
+/* End of file test.php */
+/* Location: ./application/controllers/test.php */
