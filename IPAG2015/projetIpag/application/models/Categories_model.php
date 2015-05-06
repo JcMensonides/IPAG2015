04caa0c52c6d2728cbb4e2c06b4a4fef5676442c
JC <JC@JC-PC>
2015-05-06 13:56:55 +0200

    ça commence

---
+++ IPAG2015/projetIpag/application/models/Categories_model.php
@@ -0,0 +1,20 @@
+<?php
+class News_model extends CI_Model {
+
+        public function __construct()
+        {
+                $this->load->database();
+        }
+        
+        public function get_news($slug = FALSE)
+        {
+        	if ($slug === FALSE)
+        	{
+        		$query = $this->db->get('etudiant');
+        		return $query->result_array();
+        	}
+        
+        	$query = $this->db->get_where('etudiant', array('Nom' => "1"));
+        	return $query->row_array();
+        }
+}
