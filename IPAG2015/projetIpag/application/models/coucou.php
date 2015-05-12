71754c5a5c9efdafc2b0412286a181deddade1a2
JC <JC@JC-PC>
2015-05-12 14:47:02 +0200

    presque finit la vue "ajout edition"

--- IPAG2015/projetIpag/application/models/coucou.php
+++ IPAG2015/projetIpag/application/models/coucou.php
@@ -1,57 +1,96 @@
-body {
-  padding-top: 40px;
-  background-color: #f5f5f5;
-}
-.flat .plan {
-  border-radius: 6px;
-  list-style: none;
-  padding: 0 0 20px;
-  margin: 0 0 15px;
-  background: #fff;
-  text-align: center;
-}
-.flat .plan li {
-  padding: 10px 15px;
-  color: #333;
-  border-top: 1px solid #f5f5f5;
-  -webkit-transition: 300ms;
-  transition: 300ms;
-}
-.flat .plan li.plan-price {
-  border-top: 0;
-}
-.flat .plan li.plan-name {
-  border-radius: 6px 6px 0 0;
-  padding: 15px;
-  font-size: 24px;
-  line-height: 24px;
-  color: #fff;
-  background: #e74c3c;
-  margin-bottom: 30px;
-  border-top: 0;
-}
-.flat .plan li > strong {
-  color: #e74c3c;
-}
-.flat .plan li.plan-action {
-  margin-top: 10px;
-  border-top: 0;
-}
-.flat .plan.featured {
-  -webkit-transform: scale(1.1);
-  -ms-transform: scale(1.1);
-  transform: scale(1.1);
-}
-.flat .plan.featured li.plan-name {
-  background: #000;
-}
-.flat .plan.featured:hover li.plan-name {
-  background: #c0392b;
-}
-.flat .plan:hover li.plan-name {
-  background: #000;
-}
-#footer {
-  margin-top: 100px;
-  padding-bottom: 30px;
-}
+<div style="padding: 3px; width =: 24%"
+			class="col-lg-3 col-md-3 col-xs-6">
+			<ul class="plan plan1">
+				<li class="plan-name">Epreuves ecrites</li>
+
+				<li class="plan-action">
+					<div id="addEpreuveEcrite" style="display: block">
+						<div>
+							<label> Pas d'epreuve ecrite</label> <label
+								class="Ajout-round-button"
+								onclick="toggle_visibility('addEpreuveEcrite');"> + </label>
+						</div>
+					</div>
+					<div id="addEpreuveEcriteDelete" style="display: none">
+						<div class="Delete-round-button"
+							onclick="toggle_visibility('addEpreuveEcrite'); delete_all_matieres_ecrites();">X</div>
+						<br /> <label> Du</label> <input type="date"
+							class="addEpreuveEcriteDeleteClass" name="DateEpreuveEcrite" /> <br />
+						<label> au (*)</label> <input type="date"
+							class="addEpreuveEcriteDeleteClass"
+							name="DateResultatEpreuveEcrite" /> <br /> <br /> <label> Date de
+							resultat des epreuves ecrites</label> <input type="date"
+							class="addEpreuveEcriteDeleteClass"
+							name="DateResultatEpreuveEcrite" /> <br /> <br />
+							
+							
+							
+							<div id="addMatiereEcrite" style="display: block">
+							<div>
+								<label> Matieres Ecrites</label> <label
+									class="Ajout-round-button" id = "addMatiereButton"
+									onclick="add_matiere_ecrite('nouvelleMatiereEcrite')"> + </label>
+							</div>
+						</div>
+						<div id="nouvelleMatiereEcrite"></div>
+						<div id="addMatiereEcriteDelete">
+						<div class="Delete-round-button" id= "deleteMatiereButton" style= "display:none"
+							onclick="delete_matiere_ecrite();">X</div>
+						</div>
+						
+					</div>
+			
+			</ul>
+		</div>
+		
+		<script>
+		nbMatiereEcrite= 0;
+	function add_matiere_ecrite(id) {
+		nbMatiereEcrite++;
+		var deleteMatiereButton = document.getElementById("deleteMatiereButton");
+		deleteMatiereButton.style.display='block';
+		
+		var element = document.getElementById(id);
+		var matiereLabel = document.createElement("label");
+		matiereLabel.setAttribute("id", "labelEcrite" + nbMatiereEcrite);
+		matiereLabel.innerHTML = "Matiere " + nbMatiereEcrite + " :";
+		
+		var matiereInput = document.createElement("input");
+		matiereInput.setAttribute("id", "matiereEcrite" + nbMatiereEcrite);
+		matiereInput.setAttribute("placeholder", "Nom de la matiere " + nbMatiereEcrite);
+		
+
+		element.appendChild(matiereLabel);
+		element.appendChild(matiereInput);
+	}
+
+	function delete_matiere_ecrite() {
+		if(nbMatiereEcrite >0) {
+			var parent = document.getElementById("nouvelleMatiereEcrite");
+			var inputADelete = document.getElementById("matiereEcrite" + nbMatiereEcrite);
+			var labelADelete = document.getElementById("labelEcrite" + nbMatiereEcrite);
+			parent.removeChild(inputADelete);
+			parent.removeChild(labelADelete);
+			nbMatiereEcrite --;
+		}
+		if(nbMatiereEcrite == 0) {
+			var deleteMatiereButton = document.getElementById("deleteMatiereButton");
+			deleteMatiereButton.style.display='none';
+		}
+	}
+
+	function delete_all_matieres_ecrites() {
+		while(nbMatiereEcrite>0) {
+			var parent = document.getElementById("nouvelleMatiereEcrite");
+			var inputADelete = document.getElementById("matiereEcrite" + nbMatiereEcrite);
+			var labelADelete = document.getElementById("labelEcrite" + nbMatiereEcrite);
+			parent.removeChild(inputADelete);
+			parent.removeChild(labelADelete);
+			nbMatiereEcrite --;
+		}
+		var deleteMatiereButton = document.getElementById("deleteMatiereButton");
+		deleteMatiereButton.style.display='none';
+	}
+	
+		
+</script>
--- IPAG2015/projetIpag/application/views/EditionConcours/ajout_EditionConcours.php
+++ IPAG2015/projetIpag/application/views/EditionConcours/ajout_EditionConcours.php
@@ -3,15 +3,17 @@
 
 	<h1 class="page-header">Ajouter une edition de concours</h1>
 	<div class="row flat">
+	
+	<?php echo validation_errors(); ?>
+
+					<?php echo form_open('EditionConcours/ajoutEditionConcours')?>
 
 		<div style="width: 100%" class="col-lg-3 col-md-3 col-xs-6">
 			<ul class="plan plan1">
 				<li class="plan-name">Nouvelle edition</li>
 
 				<li class="plan-action">
-                    <?php echo validation_errors(); ?>
-
-					<?php echo form_open('EditionConcours/ajoutEditionConcours')?>
+                    
 						
 				
 				
@@ -82,73 +84,99 @@
 		<div style="padding: 3px; width =: 24%"
 			class="col-lg-3 col-md-3 col-xs-6">
 			<ul class="plan plan1">
-				<li class="plan-name">Nouvelle edition</li>
+				<li class="plan-name">Epreuves ecrites</li>
 
 				<li class="plan-action">
-                    <?php echo validation_errors(); ?>
-
-					<?php echo form_open('EditionConcours/ajoutEditionConcours')?>
-						
-				
-				
-				
-				
-				
-				
-				
-				<li><strong> Concours </strong> <br /> <select name=numConcours>
-							 		<?php
-										
-										$previous_libTheme = false;
-										foreach ( $listConcours as $unConcours ) {
-											if ($unConcours ['LibelleTheme'] !== $previous_libTheme) {
-												?>
-								 			<optgroup label="<?php echo $unConcours['LibelleTheme'];?>">
-								 			<?php
-												
-												$previous_libTheme = $unConcours ['LibelleTheme'];
-											}
-											?></optgroup>
-						<option value="<?php echo $unConcours['NumConcours'];?>"><?php echo $unConcours['LibelleConcours'];?></option>
-							 		<?php }?>
-							
-				
-				
-				
-				
-				
-				</select></li>
-				<li><strong> Dates d'inscriptions </strong> <br /> <br /> <label>Du
-				</label> <input type="date" placeholder="JJ/MM/AAAA"
-					name="dateDebutInscriptionEditionConcours"> <label> au (*)</label>
-					<input type="date" placeholder="JJ/MM/AAAA"
-					name="dateFinInscriptionEditionConcours"></li>
-
-				<li><strong> Date de resultats du concours(*) </strong> <br /> <input
-					type="date" placeholder="JJ/MM/AAAA"
-					name="dateResultatsEditionConcours"></li>
-				<li><strong>QCM</strong> <br />
-					<div id="addQCM" style="display: block">
+					<div id="addEpreuveEcrite" style="display: block">
 						<div>
-							<label class="Ajout-round-button"
-								onclick="toggle_visibility('addQCM');"> + </label>
+							<label> Pas d'epreuve ecrite</label> <label
+								class="Ajout-round-button"
+								onclick="toggle_visibility('addEpreuveEcrite');"> + </label>
 						</div>
 					</div>
-					<div id="addQCMDelete" style="display: none">
+					<div id="addEpreuveEcriteDelete" style="display: none">
 						<div class="Delete-round-button"
-							onclick="toggle_visibility('addQCM');">X</div>
-						<br /> <label> Date du QCM</label> <input type="date"
-							class="cachable" name="DateQCM" /> <label> Date Resultat du QCM</label>
-						<input type="date" class="cachable" name="DateResultatQCM" />
+							onclick="toggle_visibility('addEpreuveEcrite'); delete_all_matieres_ecrites();">X</div>
+						<br /> <label> Du</label> <input type="date"
+							class="addEpreuveEcriteDeleteClass" name="DateEpreuveEcrite" /> <br />
+						<label> au (*)</label> <input type="date"
+							class="addEpreuveEcriteDeleteClass"
+							name="DateResultatEpreuveEcrite" /> <br /> <br /> <label> Date de
+							resultat des epreuves ecrites</label> <input type="date"
+							class="addEpreuveEcriteDeleteClass"
+							name="DateResultatEpreuveEcrite" /> <br /> <br />
+							
+							
+							
+							<div id="addMatiereEcrite" style="display: block">
+							<div>
+								<label> Matieres Ecrites</label> <label
+									class="Ajout-round-button" id = "addMatiereButton"
+									onclick="add_matiere_ecrite('nouvelleMatiereEcrite')"> + </label>
+							</div>
+						</div>
+						<div id="nouvelleMatiereEcrite"></div>
+						<div id="addMatiereEcriteDelete">
+						<div class="Delete-round-button" id= "deleteMatiereButton" style= "display:none"
+							onclick="delete_matiere_ecrite();">X</div>
+						</div>
+						
 					</div>
-					<div></div></li>
-				<li><input class="btn btn-danger btn-lg" type="submit"
-					name="ajout_EditionConcours" value="Ajouter" />
-					</form></li>
-
-
+			
 			</ul>
 		</div>
+		
+		<script>
+		nbMatiereEcrite= 0;
+	function add_matiere_ecrite(id) {
+		nbMatiereEcrite++;
+		var deleteMatiereButton = document.getElementById("deleteMatiereButton");
+		deleteMatiereButton.style.display='block';
+		
+		var element = document.getElementById(id);
+		var matiereLabel = document.createElement("label");
+		matiereLabel.setAttribute("id", "labelEcrite" + nbMatiereEcrite);
+		matiereLabel.innerHTML = "Matiere " + nbMatiereEcrite + " :";
+		
+		var matiereInput = document.createElement("input");
+		matiereInput.setAttribute("id", "matiereEcrite" + nbMatiereEcrite);
+		matiereInput.setAttribute("placeholder", "Nom de la matiere " + nbMatiereEcrite);
+		
+
+		element.appendChild(matiereLabel);
+		element.appendChild(matiereInput);
+	}
+
+	function delete_matiere_ecrite() {
+		if(nbMatiereEcrite >0) {
+			var parent = document.getElementById("nouvelleMatiereEcrite");
+			var inputADelete = document.getElementById("matiereEcrite" + nbMatiereEcrite);
+			var labelADelete = document.getElementById("labelEcrite" + nbMatiereEcrite);
+			parent.removeChild(inputADelete);
+			parent.removeChild(labelADelete);
+			nbMatiereEcrite --;
+		}
+		if(nbMatiereEcrite == 0) {
+			var deleteMatiereButton = document.getElementById("deleteMatiereButton");
+			deleteMatiereButton.style.display='none';
+		}
+	}
+
+	function delete_all_matieres_ecrites() {
+		while(nbMatiereEcrite>0) {
+			var parent = document.getElementById("nouvelleMatiereEcrite");
+			var inputADelete = document.getElementById("matiereEcrite" + nbMatiereEcrite);
+			var labelADelete = document.getElementById("labelEcrite" + nbMatiereEcrite);
+			parent.removeChild(inputADelete);
+			parent.removeChild(labelADelete);
+			nbMatiereEcrite --;
+		}
+		var deleteMatiereButton = document.getElementById("deleteMatiereButton");
+		deleteMatiereButton.style.display='none';
+	}
+	
+		
+</script>
 
 		<div style="padding: 3px; width =: 24%"
 			class="col-lg-3 col-md-3 col-xs-6">
@@ -165,7 +193,7 @@
 					</div>
 					<div id="addEpreuveOraleDelete" style="display: none">
 						<div class="Delete-round-button"
-							onclick="toggle_visibility('addEpreuveOrale');">X</div>
+							onclick="toggle_visibility('addEpreuveOrale'); delete_all_matieres_orales();">X</div>
 						<br /> <label> Du</label> <input type="date"
 							class="addEpreuveOraleDeleteClass" name="DateEpreuveOrale" /> <br />
 						<label> au (*)</label> <input type="date"
@@ -179,28 +207,73 @@
 							
 							<div id="addMatiereOrale" style="display: block">
 							<div>
-								<label> Pas d'epreuve orale</label> <label
-									class="Ajout-round-button"
-									onclick="toggle_visibility('addMatiereOrale');"> + </label>
+								<label> Matieres Orales</label> <label
+									class="Ajout-round-button" id = "addMatiereButton"
+									onclick="add_matiere_orale('nouvelleMatiereOrale')"> + </label>
 							</div>
 						</div>
-						<div id="addMatiereOraleDelete" style="display: none">
-							<div class="Delete-round-button"
-								onclick="toggle_visibility('addMatiereOrale');">X</div>
-							<br /> <label> Du</label> <input type="date"
-								class="addMatiereOraleDeleteClass" name="DateMatiereOrale" /> <br />
-							<label> au (*)</label> <input type="date"
-								class="addMatiereOraleDeleteClass"
-								name="DateResultatMatiereOrale" /> <br /> <br /> <label> Date de
-								resultat des epreuves orales</label> <input type="date"
-								class="addEMatiereOraleDeleteClass"
-								name="DateResultatMatiereOrale" /> <br /> <br />
+						<div id="nouvelleMatiereOrale"></div>
+						<div id="addMatiereOraleDelete">
+						<div class="Delete-round-button" id= "deleteMatiereButton" style= "display:none"
+							onclick="delete_matiere_orale();">X</div>
 						</div>
-					
+						
 					</div>
 			
 			</ul>
 		</div>
+		
+		<script>
+		nbMatiereOrale= 0;
+	function add_matiere_orale(id) {
+		nbMatiereOrale++;
+		var deleteMatiereButton = document.getElementById("deleteMatiereButton");
+		deleteMatiereButton.style.display='block';
+		
+		var element = document.getElementById(id);
+		var matiereLabel = document.createElement("label");
+		matiereLabel.setAttribute("id", "labelOrale" + nbMatiereOrale);
+		matiereLabel.innerHTML = "Matiere " + nbMatiereOrale + " :";
+		
+		var matiereInput = document.createElement("input");
+		matiereInput.setAttribute("id", "matiereOrale" + nbMatiereOrale);
+		matiereInput.setAttribute("placeholder", "Nom de la matiere " + nbMatiereOrale);
+		
+
+		element.appendChild(matiereLabel);
+		element.appendChild(matiereInput);
+	}
+
+	function delete_matiere_orale() {
+		if(nbMatiereOrale >0) {
+			var parent = document.getElementById("nouvelleMatiereOrale");
+			var inputADelete = document.getElementById("matiereOrale" + nbMatiereOrale);
+			var labelADelete = document.getElementById("labelOrale" + nbMatiereOrale);
+			parent.removeChild(inputADelete);
+			parent.removeChild(labelADelete);
+			nbMatiereOrale --;
+		}
+		if(nbMatiereOrale == 0) {
+			var deleteMatiereButton = document.getElementById("deleteMatiereButton");
+			deleteMatiereButton.style.display='none';
+		}
+	}
+
+	function delete_all_matieres_orales() {
+		while(nbMatiereOrale>0) {
+			var parent = document.getElementById("nouvelleMatiereOrale");
+			var inputADelete = document.getElementById("matiereOrale" + nbMatiereOrale);
+			var labelADelete = document.getElementById("labelOrale" + nbMatiereOrale);
+			parent.removeChild(inputADelete);
+			parent.removeChild(labelADelete);
+			nbMatiereOrale --;
+		}
+		var deleteMatiereButton = document.getElementById("deleteMatiereButton");
+		deleteMatiereButton.style.display='none';
+	}
+	
+		
+</script>
 
 
 
@@ -210,9 +283,7 @@
 				<li class="plan-name">Nouvelle edition</li>
 
 				<li class="plan-action">
-                    <?php echo validation_errors(); ?>
-
-					<?php echo form_open('EditionConcours/ajoutEditionConcours')?>
+                   
 						
 				
 				
@@ -269,9 +340,7 @@
 						<input type="date" class="cachable" name="DateResultatQCM" />
 					</div>
 					<div></div></li>
-				<li><input class="btn btn-danger btn-lg" type="submit"
-					name="ajout_EditionConcours" value="Ajouter" />
-					</form></li>
+				
 
 
 			</ul>
@@ -284,6 +353,7 @@
 
 		<input style="width: 100%" class="btn btn-danger btn-lg" type="submit"
 			name="ajout_EditionConcours" value="Ajouter" />
+			</form>
 
 
 
@@ -313,6 +383,8 @@
     }
 //-->
 </script>
+
+
 
 
 
