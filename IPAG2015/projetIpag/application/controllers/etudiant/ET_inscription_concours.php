504dd5ac3fcd4788e3fa8973421c282a9f048cbc
JC <JC@JC-PC>
2015-05-23 15:49:58 +0200

    Synchronisation des resultats QCM, ecrits et oraux (pas mais de limite de date pour l'instant)

--- IPAG2015/projetIpag/application/controllers/etudiant/ET_inscription_concours.php
+++ IPAG2015/projetIpag/application/controllers/etudiant/ET_inscription_concours.php
@@ -73,10 +73,40 @@ class ET_inscription_concours extends CI_Controller {
 		
 		public function updateInfos() {
 			if($this->session->userdata('numEtudiant')!=="admin") {
-				$this->ET_inscription_concours_model->updateInfos();
+				//callback sur l'ordre des concours
+				//On ne peut etre admis a l'ecrit que si on est admis au qcm
+				//On ne peut etre admis a l'oral que si on est admis a l'ecrit
+				$this->form_validation->set_rules('ordreConcoursValide', 'ordre concours valide', 'callback_ordreConcoursValide');
+				if($this->form_validation->run() == false){
+					$this->renseigner();
+				}
+				else {
+					$this->ET_inscription_concours_model->updateInfos();
+					$this->mesConcours();
+				}
 				
-				$this->mesConcours();
 			}
+		}
+		
+		//Vérifie que l'on est bien admis au qcm avant d'etre admis aux ecrits
+		//Vérifie que l'on est bien admis aux ecrits avant d'etre admis aux oraux
+		public function ordreConcoursValide() {
+			$result = true;
+			//si il y a un QCM et que l'etudiant n'est pas admis
+			if(isset($_POST['admisQCM']) && $_POST['admisQCM'] !=='admis'){
+				if((isset($_POST['admisEpreuvesEcrites']) && $_POST['admisEpreuvesEcrites'] !=='non renseigne') || (isset($_POST['admisEpreuvesOrales']) && $_POST['admisEpreuvesOrales'] !=='non renseigne')){
+					$this->form_validation->set_message('ordreConcoursValide', 'Vous devez etre admis au qcm pour renseigner les ecrits ou les oraux');
+					$result= false;
+				}
+			}
+			//sinon si il y a une epreuve ecrite et que l'etudiant n'est pas admis
+			elseif(isset($_POST['admisEpreuvesEcrites']) && $_POST['admisEpreuvesEcrites'] !=='admis'){
+				if(isset($_POST['admisEpreuvesOrales']) && $_POST['admisEpreuvesOrales'] !=='non renseigne'){
+					$this->form_validation->set_message('ordreConcoursValide', 'Vous devez etre admis aux ecrits pour renseigner les oraux');
+					$result= false;
+				}
+			}
+			return $result;
 		}
 }
 		
--- IPAG2015/projetIpag/application/views/etudiant/ET_renseigner.php
+++ IPAG2015/projetIpag/application/views/etudiant/ET_renseigner.php
@@ -1,3 +1,4 @@
+<?php echo validation_errors(); ?>
 <?php echo form_open('etudiant/ET_inscription_concours/updateInfos')?>
 <div class="container">
 
